4a4c79617712130b7d1b04edbedeac1d36e2dc01
Merla Jean Ner <merlajeanner02@gmail.com>
2024-04-09 13:36:59 +0800

    udpate1

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\WebinarController;
 use App\Http\Controllers\EducationController;
 use App\Http\Controllers\ExperienceController;
-use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\FrontEndController;
 use App\Http\Controllers\SkillController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
@@ -17,7 +17,7 @@ use App\Http\Controllers\Auth\LoginController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register web add .routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
@@ -28,7 +28,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/', [App\Http\Controllers\FrontendController::class, 'index'])->name('users');
+Route::get('/', [App\Http\Controllers\FrontEndController::class, 'index'])->name('users');
 Route::resource('users', UserController::class);
 Route::resource('skills', SkillController::class);
 Route::resource('experiences', ExperienceController::class);
